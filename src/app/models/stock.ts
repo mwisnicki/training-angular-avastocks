7e77a9e7291b3b0ccc9748baa45cf1acd94b077b
Marcin Wisnicki <mwisnicki@gmail.com>
2020-08-14 20:54:21 -0400

    Cleanup

--- src/app/models/stock.ts
+++ src/app/models/stock.ts
@@ -9,9 +9,9 @@ export interface Stock {
 export type StockSymbol = string;
 
 export interface StockTick {
-  stock: StockSymbol; // symbol
+  stock: StockSymbol;
   price: number;
-  date: string; // 2020-07-24T15:36:14.226Z
+  date: ISODateString;
 }
 
 export interface StockPricePoint {
